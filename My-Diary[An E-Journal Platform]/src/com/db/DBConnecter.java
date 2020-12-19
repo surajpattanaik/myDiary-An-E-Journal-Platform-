6548f061940db2a6c56b149ac6a8eb7418e5e15c
surajpattanaik <surajkumarpattanaik3@gmail.com>
2020-12-19 17:32:57 +0530

    getting mySQL connection from Connection pool(JNDI registry)

--- My-Diary[An E-Journal Platform]/src/com/db/DBConnecter.java
+++ My-Diary[An E-Journal Platform]/src/com/db/DBConnecter.java
@@ -3,6 +3,9 @@ package com.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 public class DBConnecter {
 
 	private static Connection conn;
@@ -10,10 +13,15 @@ public class DBConnecter {
 	public static Connection getConn() {
 		try {
 			if (conn == null) {
+				/*
 				  //register jdbc driver s/w (optional)
 				 Class.forName("com.mysql.jdbc.Driver");
 				 //establish the connection
 				 conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "illusio9");
+				 */
+				InitialContext ic=new InitialContext();
+				DataSource ds=(DataSource) ic.lookup("java:/comp/env/DsJndi");
+				conn=ds.getConnection();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
